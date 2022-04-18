2a9bf9fad2c644357f7708cbaf3aae818620446f
Jose-Borges <80893822+Jose-Borges@users.noreply.github.com>
2022-04-18 15:27:40 +0100

    Add files via upload

---
+++ Trabalho1/insertTable.sql
@@ -0,0 +1,4 @@
+insert into Cliente values(123456789, 'joca', 'casa do joca', 123456789, 'I', B'1')
+
+insert into particulares values (123456789, '12345678 9 AB3')
+
---
+++ Trabalho1/removeTable.sql
@@ -0,0 +1,11 @@
+drop table if exists Cliente cascade;
+
+drop table if exists Particulares cascade;
+
+drop table if exists Institucionais cascade;
+
+drop table if exists Veiculo cascade;
+
+drop table if exists Zona_Verde cascade;
+
+drop table if exists GPS cascade;
