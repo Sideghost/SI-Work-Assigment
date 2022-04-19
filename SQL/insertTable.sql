4281991fa4a6a35c102655ca44b26f219c4c276a
sideghost <guilherme.cordeiro.pereira@gmail.com>
2022-04-19 13:50:07 +0100

    insert values half done

--- SQL/insertTable.sql
+++ SQL/insertTable.sql
@@ -27,3 +27,49 @@ insert into Cliente values (12, 'pardal(ito)', 'm10', 123456789, 'i', B'1');
 
 --Veiculo----------------------------------------------------------
 
+insert into Veiculo('AA16AA', 'agnaldo', 123456789, 12, 10);
+
+insert into Veiculo('4572XQ', 'tiburcio', 987654321, 9);
+
+insert into Veiculo('7293FQ', 'armagedon', 456789123, 3);
+
+insert into Veiculo('PG0870', 'gertrudes', 789456123,5);
+
+insert into Veiculo('00AA00', 'binte', 789123465, 5);
+
+insert into Veiculo('AA08AM', 'botaABaixo', 456123789, 8);
+
+insert into Veiculo('AA01AA', 'carneAssada', 000000000, -10);
+
+--ZONA VERDE---------------------------------------------------------
+
+insert into Zona_Verde(123456789, 7, 1);
+
+insert into Zona_Verde(987654321, 7, 1);
+
+insert into Zona_Verde(456789123, 7, 1);
+
+insert into Zona_Verde(789456123, 7, 1);
+
+insert into Zona_Verde(789123465, 7, 1);
+
+insert into Zona_Verde(456123789, 7, 1);
+
+insert into Zona_Verde(000000000, 7, 1);
+
+--GPS---------------------------------------------------------------------
+
+insert into GPS(1, 1, '', 123456789);
+
+insert into GPS(2, 1, '', 987654321);
+
+insert into GPS(3, 1, '', 456789123);
+
+insert into GPS(4, 1, '', 789456123);
+
+insert into GPS(5, 1, '', 789123465);
+
+insert into GPS(6, 1, '', 456123789);
+
+insert into GPS(7, 1, '', 000000000);
+
