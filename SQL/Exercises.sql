11986f9091983233e022da4691869ab9161432c5
Jose-Borges <80893822+Jose-Borges@users.noreply.github.com>
2022-05-06 10:53:07 +0100

    Update Exercises.sql

--- SQL/Exercises.sql
+++ SQL/Exercises.sql
@@ -1,23 +1,23 @@
 --1.d)
 
-CREATE procedure insert_particular(nif varchar,nome varchar,morada varchar,telefone varchar,ativo bit,referencia varchar,cc varchar)    
+create procedure insert_particular(nif varchar, nome varchar, morada varchar, telefone varchar, referencia varchar, cc varchar)    
     LANGUAGE plpgsql
 as
 $$
-    BEGIN    
-        INSERT INTO cliente values (nif,nome,morada,telefone,'P',ativo,referencia);
-        INSERT INTO particulares VALUES (nif,cc);
-        if(nif not in (SELECT cliente.nif FROM cliente)) then
-       		raise notice 'Cliente n�o inserido';        
-    	END IF;
+    begin    
+        insert into cliente values (nif,nome,morada,telefone,'P',referencia);
+        insert into particulares values (nif,cc);
+        if(nif not in (select cliente.nif from cliente)) then
+       		raise notice 'Cliente não inserido';        
+    	end if;
     end;
 $$;
     
-drop procedure if exists insert_particular(nif varchar,nome varchar,morada varchar, telefone varchar,tipo varchar,ativo bit,referencia varchar,cc varchar); 
+drop procedure if exists insert_particular(nif varchar, nome varchar, morada varchar, telefone varchar, referencia varchar, cc varchar); 
 
-call insert_particular('123456799' ,'FCPORCO' ,'Rua abcd','967995934' ,B'1',null,'15111660'); 
-
-CREATE procedure update_particular(NIF_ varchar, new_nome varchar, new_morada varchar, new_telefone varchar, new_ativo bit)--ativo poderia estar como um varchar
+call insert_particular('123456791', 'FCPORCO', 'Rua abcd', '967995934', null, '15111660'); 
+------------------------------------------------------------------------------------------------------------------------
+create procedure update_particular(NIF_ varchar, new_nome varchar, new_morada varchar, new_telefone varchar, new_ativo bit)--ativo poderia estar como um varchar
     LANGUAGE plpgsql
 as
 $$
@@ -39,48 +39,120 @@ $$;
 
 drop procedure if exists update_particular(nif_ varchar,nome_ varchar,morada_ varchar, telefone_ varchar, ativo_ bit);
 
-call update_particular('123456779' ,'DJDODIA' , null , null, B'1');
-
--- 1.h) --dica pardal(ito) -> usar cursor
-
-create procedure add_veicule_to_green_zone(green_zone_id integer, zone_radius integer, zone_gps_coords integer, matricula varchar)
-    language plpgsql
+call update_particular('123456791' ,'DJDODIA' , null , null, B'0');
+------------------------------------------------------------------------------------------------------------------------
+create Procedure remove_particular(nif_ varchar)
+    LANGUAGE plpgsql
 as
 $$
-    begin
-        if ((green_zone_id is not null) and (zone_radius is not null) and (zone_gps_coords is not null)) then
-            insert into Zona_Verde values(green_zone_id, zone_radius, zone_gps_coords, matricula);
+declare 
+        ativoCurr bit;
+    begin    
+        update cliente set ativo = B'0' where NIF = nif_;
+        select cliente.ativo  from cliente where (NIF = nif_) into ativoCurr;
+        if(ativoCurr <> B'0') then
+            raise notice 'Cliente não removido';
         end if;
-        if (green_zone_id not in (select zona_verde.green_zone_id from veiculo)) then 
-            raise notice 'Veiculo nao associado a Zona Verde';
-        end if;
-    end;
+        end;
 $$;
+
+drop procedure if exists remove_particular(nif_ varchar);
+
+call remove_particular('123456780'); 
+
+--1.e)
+
+create function number_of_alarms(ano integer,matricula_ varchar = null)
+	returns integer
+	language plpgsql
+as
+$$
+	begin
+		if (matricula_ is not null) then
+			return count(matricula) numero_alarmes from Alarmes join GPS on id_gps = GPS.id where (matricula = matricula_ and extract(year from GPS.marca_temporal) = ano);
+		else
+			return count(matricula) numero_alarmes from Alarmes join GPS on id_gps = GPS.id;
+		end if;
+	end;
+$$
+
+drop function if exists number_of_alarms(ano integer, nif_ varchar);
+
+select number_of_alarms(2022, 'AE12ER')
+
+--1.f)
+
+create procedure process_registers()
+	language plpgsql
+as
+$$
+declare
+		id_zona_ integer;
+		id_GPS_	 integer;
+	begin 
+		select id_zona from Registos_Nao_Processados 
+ 	end;
+$$
+
+drop procedure process_registers()
+		
+--h)
 
 create procedure add_vehicle_to_client_or_not(matricula varchar, driver varchar, phone_driver varchar, client_nif varchar, green_zone_id integer, zone_radius integer, zone_gps_coords integer)
     language plpgsql
 as
 $$
     begin 
-        if (client_nif exists in (select cliente.nif from cliente)) then
-            if(not (client_nif in(select cliente.tipo = 'P' from cliente))) then
+        if (client_nif in (select cliente.nif FROM cliente)) then
+            if(not (client_nif in(select cliente.tipo = 'P'))) then
                 insert into Veiculo values(matricula, driver, phone_driver, client_nif, null);
-                call add_veicule_to_green_zone(green_zone_id, zone_radius, zone_gps_coords, matricula);
+                if ((green_zone_id is not null) and (zone_radius is not null) and (zone_gps_coords is not null)) then
+                    insert into Zona_Verde values(green_zone_id, zone_radius, zone_gps_coords, matricula);
+                end if;
             else 
-                if(select count(cliente.nif = Veiculo.nif) < 3) then
+                if(count(client.nif = Veiculo.nif) < 3) then
                     insert into Veiculo values(matricula, driver, phone_driver, client_nif, 0);
-                    call add_veicule_to_green_zone(green_zone_id, zone_radius, zone_gps_coords, matricula);
+                    if ((green_zone_id is not null) and (zone_radius is not null) and (zone_gps_coords is not null)) then
+                        insert into Zona_Verde values (green_zone_id, zone_radius, zone_gps_coords, matricula);
+                    end if;
                 end if;
             end if;
         end if;
         if (matricula not in (select veiculo.matricula from veiculo)) then 
-            raise notice 'Veiculo nao inserido';
+            raise notice 'Veiculo não inserido';
         end if;
     end;
 $$;
 
-drop procedure if exists add_vehicle_to_client_or_not(matricula varchar, driver varchar, phone_driver varchar, client_nif varchar, green_zone_id integer, zone_radius integer, zone_gps_coords integer);
+drop procedure if exists add_vehicle_to_client_or_not(matricula varchar, driver varchar, phone_driver varchar, client_nif varchar, green_zone_id integer, zone_radius integer, zone_gps_coords integer)
 
-call add_vehicle_to_client_or_not('BBBBBB', 'Josevaldo das caricas', 967995999, '123456779', 6, 500, 10);
+call add_vehicle_to_client_or_not('BBBBBB','Josevaldo das caricas',967995999,123456779,6,500,10) 
 
-call add_veicule_to_green_zone(6, 500, 10, 'BBBBBB');
+--i)
+
+create view alarmes as 
+	select Veiculo.matricula, nome_condutor, latitude, longitude, Alarmes.marca_temporal as dia_hora 
+		from Veiculo 
+			join GPS
+			on (Veiculo.matricula = GPS.matricula)
+			join Alarmes
+			on (Alarmes.id_GPS = GPS.id)
+			group by Veiculo.matricula;
+			
+drop view if exists alarmes;
+
+--k)
+	
+create procedure eliminate_invalid_registers() 
+	language plpgsql
+as
+$$
+	begin 
+		delete from Registos_Invalidos where (DATEDIFF('day', NOW()::timestamp, marca_temporal) >= 15);
+	end;
+$$;
+	
+--select from Registos_Invalidos where (DATE_PART('day', '2014-07- 06:14:00.742000000'::timestamp - marca_temporal::timestamp)>=15);
+drop procedure if exists eliminate_invalid_registers();
+
+call eliminate_invalid_registers() 
